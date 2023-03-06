301bca75119b1950c297d254646e1a84ce381402
faturarkansyawalva <118005785+faturarkansyawalva@users.noreply.github.com>
2023-03-06 19:57:53 +0700

    Delete 121140229_Fatur Arkan Syawalva_Prak1_2.py

--- 121140229_Fatur Arkan Syawalva_Prak1_2.py
+++
@@ -1,8 +0,0 @@
-lebar_persegi = int(input("masukan lebar persegi :"))
-panjang_persegi = int(input("masukan panjang persegi :"))
-
-
-for i in range (panjang_persegi):
-    for j in range (lebar_persegi):
-        print(" *", end="")
-    print()   
